e43d9c7ea76fc0418949e8c5b6d827952419b3a2
shamilov <sshamillov@gmail.com>
2024-01-27 11:07:55 +0600

    add settings configuration component

---
+++ src/main/kotlin/com/shamilov/prayer/config/PrayerConfigurable.kt
@@ -0,0 +1,78 @@
+package com.shamilov.prayer.config
+
+import com.intellij.openapi.options.Configurable
+import com.shamilov.prayer.data.repository.CountriesRepository
+import com.shamilov.prayer.data.repository.TimingsRepository
+import com.shamilov.prayer.persistence.location.LocationStore
+import com.shamilov.prayer.persistence.timings.TimingsStore
+import com.shamilov.prayer.scheduler.ReloadTimingsScheduler
+import javax.swing.JComponent
+
+/**
+ * @author Shamilov on 16.01.2024
+ */
+internal class PrayerConfigurable : Configurable {
+
+    private companion object {
+        const val DISPLAY_NAME = "Prayer"
+    }
+
+    private val timingsRepository = TimingsRepository()
+    private val countriesRepository = CountriesRepository()
+    private val locationStore = LocationStore.instance
+
+    private val component = PrayerSettingsComponent()
+
+    init {
+        component.setCountries(countriesRepository.getCountries())
+        component.setCity(locationStore.city)
+        component.setCountry(locationStore.country)
+        TimingsStore.instance.state.timings?.let {
+            component.setTimings(it)
+        }
+    }
+
+    override fun createComponent(): JComponent {
+        return component.getComponent()
+    }
+
+    override fun isModified(): Boolean {
+        val city = locationStore.city.orEmpty()
+        val country = locationStore.country.orEmpty()
+
+        return component.getCity()!= city || component.getCountry() != country
+    }
+
+    override fun reset() {
+        component.reset(
+            locationStore.city,
+            locationStore.country
+        )
+    }
+
+    override fun apply() {
+        component.setLoading(true)
+
+        val city = component.getCity()
+        val country = component.getCountry()
+
+        locationStore.city = city
+        locationStore.country = country
+
+        ReloadTimingsScheduler.schedule(city, country)
+
+        timingsRepository.loadLimits(
+            city = component.getCity(),
+            country = component.getCountry(),
+        ) { result ->
+            component.setLoading(false)
+            result.onSuccess { timings ->
+                component.setTimings(timings.timingsOfDay)
+            }
+        }
+    }
+
+    override fun getDisplayName(): String {
+        return DISPLAY_NAME
+    }
+}
---
+++ src/main/kotlin/com/shamilov/prayer/config/PrayerSettingsComponent.kt
@@ -0,0 +1,103 @@
+package com.shamilov.prayer.config
+
+import com.intellij.openapi.ui.ComboBox
+import com.intellij.ui.components.JBLabel
+import com.shamilov.prayer.entity.Countries
+import com.shamilov.prayer.entity.Timing
+import com.shamilov.prayer.entity.Timings
+import org.jdesktop.swingx.VerticalLayout
+import java.awt.FlowLayout
+import java.awt.Font
+import java.awt.GridLayout
+import javax.swing.*
+
+/**
+ * @author Shamilov on 16.01.2024
+ */
+internal class PrayerSettingsComponent {
+
+    private val mainComponent = JPanel()
+    private val settingsPanel = JPanel()
+    private val timingPanel = JPanel()
+    private val cityTextField = JTextField(10)
+    private val countriesComboBox = ComboBox<String>()
+    private val progressBar = JProgressBar().apply {
+        isVisible = false
+    }
+    private val dateLabel = JBLabel("Prayer schedule for today:").apply {
+        isVisible = false
+        font = Font("Date", Font.PLAIN, 14)
+    }
+
+    init {
+        configureSettingsPanel()
+        configureTimingPanel()
+
+        mainComponent.apply {
+            layout = VerticalLayout(20)
+            add(settingsPanel)
+            add(progressBar)
+            add(dateLabel)
+            add(timingPanel)
+        }
+    }
+
+    fun getComponent(): JComponent = mainComponent
+
+    fun setTimings(timings: List<Timing?>) {
+        progressBar.isVisible = false
+
+        if (timings.isNotEmpty()) {
+            dateLabel.isVisible = true
+            dateLabel.text = "Prayer schedule for today ():"
+            timingPanel.removeAll()
+
+            timings.filterNotNull().forEach { timing ->
+                timingPanel.add(JBLabel(timing.name))
+                timingPanel.add(JBLabel(timing.time))
+            }
+            timingPanel.updateUI()
+        }
+    }
+
+    fun setCountries(countries: Countries) {
+        countries.countries.forEach {
+            countriesComboBox.addItem(it.name)
+        }
+    }
+
+    fun reset(city: String?, country: String?) {
+        cityTextField.text = city
+        countriesComboBox.selectedItem = country
+    }
+
+    fun setLoading(visible: Boolean) {
+        progressBar.isVisible = visible
+    }
+
+    fun getCity(): String = cityTextField.text.orEmpty()
+
+    fun setCity(city: String?) {
+        cityTextField.text = city
+    }
+
+    fun getCountry(): String = countriesComboBox.item.orEmpty()
+
+    fun setCountry(country: String?) {
+        countriesComboBox.item = country
+    }
+
+    private fun configureSettingsPanel() = with(settingsPanel) {
+        layout = FlowLayout(FlowLayout.LEADING, 10, 10)
+
+        add(JBLabel("City:"))
+        add(cityTextField)
+        add(JBLabel("Country:"))
+        add(countriesComboBox)
+    }
+
+    private fun configureTimingPanel() {
+        timingPanel.add(Box.createVerticalStrut(10))
+        timingPanel.layout = GridLayout(5, 2, 0, 20)
+    }
+}
