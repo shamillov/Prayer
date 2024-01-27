2e50e5535bd5dbb7f927fa4e60bf0b674f772451
GitHub Action <action@github.com>
2024-01-27 03:51:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/shamillov/prayer/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/shamillov/prayer/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.shamillov.prayer.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.shamillov.prayer.MyBundle
+import com.github.shamillov.prayer.services.MyProjectService
 import javax.swing.JButton
 
 
